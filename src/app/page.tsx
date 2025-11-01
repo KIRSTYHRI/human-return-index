9622fb0108722d73b876e8f0b69e3ab8ea32dec6
KIRSTYHRI <kirsty@workitwell.com>
2025-11-01 14:46:26 +0000

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main style={{padding: 24}}>
+      <h1>Human Return Index™</h1>
+      <p>Current Badge: <b>HRI Accredited</b></p>
+      <p>Organisation ID: 9499b1b9-7fce-43a1-9590-d533f00dc71d</p>
+      <p>Awarded: 2025-11-01</p>
+    </main>
+  )
+}
