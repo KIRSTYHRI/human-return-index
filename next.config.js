2143a95cd0ed2d48d09730cc7ff39fe7a9040dc0
KIRSTYHRI <kirsty@workitwell.com>
2025-11-02 10:23:56 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    // Allow production builds to succeed even if there are type errors.
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Skip ESLint during Vercel builds.
-    ignoreDuringBuilds: true,
-  },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
 };
 module.exports = nextConfig;
