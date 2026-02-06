0983cca152f2127e53d8fec04fd0e364619b8c8b
KIRSTYHRI <kirsty@workitwell.com>
2026-02-06 20:50:52 +0000

    Update client.js

--- src/lib/supabase/client.js
+++ src/lib/supabase/client.js
@@ -1,21 +1,21 @@
 import { createClient } from "@supabase/supabase-js";
 
-let browserClient;
+let client;
 
 export function supabaseBrowser() {
-  if (browserClient) return browserClient;
+  if (client) return client;
 
   const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
   const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-  browserClient = createClient(url, anon, {
+  client = createClient(url, anon, {
     auth: {
       persistSession: true,
-      storageKey: "hri-auth",
       autoRefreshToken: true,
       detectSessionInUrl: true,
+      storageKey: "hri-auth",
     },
   });
 
-  return browserClient;
+  return client;
 }
