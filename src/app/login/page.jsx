d082783789d8214b69a07cc2e477637d48a955c0
KIRSTYHRI <kirsty@workitwell.com>
2026-02-08 19:03:17 +0000

    Update page.jsx

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useMemo, useState } from "react";
 import { useRouter } from "next/navigation";
-import { supabaseBrowser } from "@/lib/supabase/client";
+import { supabaseBrowser } from "../../lib/supabase/client";
 
 export const dynamic = "force-dynamic";
 
