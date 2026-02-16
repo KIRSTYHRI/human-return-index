9457242e15434c18c677dbed3878af41cbb54658
Kirsty Birch <YOUR_EMAIL_USED_ON_GITHUB>
2026-02-16 22:36:05 +0000

    fix: make apiFetch export stable (named + default)

--- src/lib/apiFetch.js
+++ src/lib/apiFetch.js
@@ -1,12 +1,14 @@
-import { supabaseBrowser } from "./supabaseBrowser";
-
 export async function apiFetch(path, options = {}) {
-  const { data } = await supabaseBrowser().auth.getSession();
-  const access_token = data?.session?.access_token || null;
+  const opts = {
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      ...(options.headers || {}),
+    },
+    credentials: "include",
+  };
 
-  const headers = new Headers(options.headers || {});
-  if (!headers.has("Content-Type") && options.body) headers.set("Content-Type", "application/json");
-  if (access_token) headers.set("Authorization", `Bearer ${access_token}`);
-
-  return fetch(path, { ...options, headers, cache: "no-store" });
+  return fetch(path, opts);
 }
+
+export default apiFetch;
